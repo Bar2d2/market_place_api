4961edbc32d8d34547b41e0e6505b5e1a50491f0
BartOz <bartek.ozdoba@gmail.com>
2022-09-28 17:08:57 +0200

    Adds the products create, update and destroy action

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,4 +1,7 @@
 class Api::V1::ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :update, :destroy]
+  before_action :check_login, only: [:create]
+  before_action :check_owner, only: [:update]
 
   def index
     render json: Product.all
@@ -6,6 +9,43 @@ class Api::V1::ProductsController < ApplicationController
 
   def show
     render json: Product.find(params[:id])
+  end
+
+  def create
+    product = current_user.products.build(product_params)
+    if product.save
+      render json: product, status: :created
+    else
+      render json: { errors: product.errors },
+      status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @product.update(product_params)
+      render json: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    head 204
+  end
+
+  private
+
+  def check_owner
+    head :forbidden unless @product.user.id == current_user&.id
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:title, :price, :published)
   end
 
 end
