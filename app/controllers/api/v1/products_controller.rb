1696462c7a41f362cfe05876a4f4a1ec1ac7977e
BartOz <bartek.ozdoba@gmail.com>
2022-09-30 15:34:13 +0200

    Adds product serializer for custom json output

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -4,17 +4,17 @@ class Api::V1::ProductsController < ApplicationController
   before_action :check_owner, only: [:update]
 
   def index
-    render json: Product.all
+    render json: ProductSerializer.new(@products).serializable_hash.to_json
   end
 
   def show
-    render json: Product.find(params[:id])
+    render json: ProductSerializer.new(@products).serializable_hash.to_json
   end
 
   def create
     product = current_user.products.build(product_params)
     if product.save
-      render json: product, status: :created
+      render json: ProductSerializer.new(products).serializable_hash.to_json, status: :created
     else
       render json: { errors: product.errors },
       status: :unprocessable_entity
@@ -23,7 +23,7 @@ class Api::V1::ProductsController < ApplicationController
 
   def update
     if @product.update(product_params)
-      render json: @product
+      render json: ProductSerializer.new(@products).serializable_hash.to_json
     else
       render json: @product.errors, status: :unprocessable_entity
     end
